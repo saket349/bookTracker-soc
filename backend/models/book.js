7922a13b129f0922bf531988c95002c0fe49fb62
sksachin28sep@gmail.com <sksachin28sep@gmail.com>
2021-07-14 13:13:20 +0530

    backend added

---
+++ backend/models/book.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const bookSchema = new schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        author: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        edition: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        isActive : {
+            type: Boolean,
+        },
+
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = Person = mongoose.model("Book", bookSchema);
